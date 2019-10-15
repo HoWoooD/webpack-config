de8840038289e5ded551720734865f2c1eb395f5
howoood <howoood@sinetx.com>
2019-10-15 14:58:40 +0900

    Exract scss to a Dedicated css file and setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 let config = {
-	entry: './index.js',
+
+	entry: './src/index.js',
 	output: {
+		path: path.resolve(__dirname, './public'),
 		filename: 'output.js'
 	},
 	module: {
-	    rules: [
+		rules: [
 		    {
 		      test: /\.js$/, // files ending with .js
 		      exclude: /node_modules/, // exclude the node_modules directory
 		      loader: "babel-loader" // use this (babel-core) loader
 		    },
 		    {
-		    	test: /\.scss$/,
-		    	loader: ['style-loader', 'css-loader', 'sass-loader']
+		      test: /\.scss$/,
+		      use: ExtractTextWebpackPlugin.extract({
+		      	use: ['css-loader', 'sass-loader'],
+		      	fallback: 'style-loader'
+		      })
 		    }
 	    ]
-  }
+	},
+	plugins: [
+		new ExtractTextWebpackPlugin('styles.css')
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname, './public'), //A directory or URL to serve HTML content from
+		historyApiFallback: true, // fallback to /index.html for Single Page Applications.
+		inline: true, //inline mode (set to false to disable including client scripts (like livereload))
+		// open: true, //open default browser while launching
+	},
+	devtool: 'eval-source-map' //enable devtool for better debugging experience
 }
 
 module.exports = config;
